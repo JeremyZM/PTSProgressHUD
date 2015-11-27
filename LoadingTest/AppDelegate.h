fd070af18315c95edd5fcf0fafd0c7013b170ae8
vjieshao <461128269@qq.com>
2015-11-27 15:58:44 +0800

    Initial Commit

---
+++ LoadingTest/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  LoadingTest
+//
+//  Created by 黄杰 on 15/11/27.
+//  Copyright © 2015年 黄杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ LoadingTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoadingTest
+//
+//  Created by 黄杰 on 15/11/27.
+//  Copyright © 2015年 黄杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
